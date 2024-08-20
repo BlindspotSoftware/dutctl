2cb2d22ef30d9c123c024c806ea42b0ee5fbbfbf
Jens Drenhaus <jens.drenhaus@9elements.com>
2024-08-20 14:30:20 +0200

    chore: move chanel I/O into own internal package

--- cmds/dutagent/session.go
+++ cmds/dutagent/session.go
@@ -3,6 +3,8 @@ package main
 import (
 	"io"
 	"log"
+
+	"github.com/BlindspotSoftware/dutctl/internal/chanio"
 )
 
 // session implements module.Session for remote sessions between the agent
@@ -24,89 +26,17 @@ func (s *session) Print(text string) {
 
 //nolint:nonamedreturns
 func (s *session) Console() (stdin io.Reader, stdout, stderr io.Writer) {
-	return &chanReader{s.stdin, []byte{}}, &chanWriter{s.stdout}, &chanWriter{s.stderr}
+	return chanio.NewChanReader(s.stdin), chanio.NewChanWriter(s.stdout), chanio.NewChanWriter(s.stderr)
 }
 
 func (s *session) RequestFile(name string) (io.Reader, error) {
 	s.fileReq <- name
 
-	return &chanReader{s.file, []byte{}}, nil
+	return chanio.NewChanReader(s.file), nil
 }
 
 func (s *session) SendFile(_ string, _ io.Reader) error {
 	log.Println("SendFile not implemented")
 
 	return nil
-}
-
-type chanReader struct {
-	ch  <-chan []byte
-	buf []byte // Buffer to store excess bytes
-}
-
-func (r *chanReader) Read(bytes []byte) (int, error) {
-	log.Printf("Channel Reader: Read called with %d bytes\n", len(bytes))
-	// If there's enough data in the buffer, use it and return early.
-	if len(r.buf) >= len(bytes) {
-		n := copy(bytes, r.buf)
-		r.buf = r.buf[n:] // Adjust the buffer
-		log.Printf("Channel Reader: Returning early (internal buffer >= read buffer), %d bytes from buffer\n", n)
-
-		return n, nil
-	}
-
-	log.Printf("Channel Reader: no early return, continue to read from buffer and channel\n")
-
-	var nBuf, nChan int
-	// If the buffer is not empty but contains some data, start by filling bytes with it.
-	if len(r.buf) > 0 {
-		log.Printf("Channel Reader: Reading from internal buffer\n")
-
-		nBuf = copy(bytes, r.buf)
-		r.buf = r.buf[nBuf:] // Adjust the buffer
-
-		if nBuf == len(bytes) {
-			// If the buffer fulfilled the bytes, return early
-			log.Printf("Channel Reader: Returning early (internal buffer = read buffer), %d bytes from buffer\n", nBuf)
-
-			return nBuf, nil
-		}
-	}
-
-	log.Printf("Channel Reader: Continue reading from channel\n")
-
-	// Read from the channel if the buffer is empty or insufficient
-	chanBytes, ok := <-r.ch
-	if !ok {
-		log.Printf("Channel Reader: Channel closed returning EOF")
-
-		return nBuf, io.EOF // Return any remaining buffer content before EOF
-	}
-
-	log.Printf("Channel Reader: Channel read %d bytes. Continue calculating total read count\n", len(chanBytes))
-
-	// Calculate the total bytes to copy to bytes, considering any existing content from the buffer
-	totalNeeded := len(bytes) - len(r.buf)
-	nChan = copy(bytes[nBuf:], chanBytes)
-
-	// If there are excess bytes, append them to the buffer
-	if totalNeeded < len(chanBytes) {
-		r.buf = append(r.buf, chanBytes[totalNeeded:]...)
-	}
-
-	log.Printf("Channel Reader: Read %d bytes from internal buffer and %d bytes from channel\n", nBuf, nChan)
-
-	return nBuf + nChan, nil
-}
-
-type chanWriter struct {
-	ch chan<- []byte
-}
-
-func (w *chanWriter) Write(bytes []byte) (int, error) {
-	chanBytes := make([]byte, len(bytes))
-	copy(chanBytes, bytes)
-	w.ch <- chanBytes
-
-	return len(bytes), nil
 }
