64ecf79af24740c0e87ea04c12c86b6663786192
Jens Drenhaus <jens.drenhaus@blindspot.software>
2024-09-21 03:52:38 +0200

    feat: dutagent runs module de-init on clean-up
    The modules Deinit function is called when:
    - a panic occurs
    - the services is terminated by the user
    - the RPC handler fails
    - module initialization fails
    
    Signed-off-by: Jens Drenhaus <jens.drenhaus@blindspot.software>

--- internal/dutagent/init.go
+++ internal/dutagent/init.go
@@ -10,11 +10,23 @@ import (
 // ModuleInitError is a container for errors that occur during module
 // initialization.
 type ModuleInitError struct {
-	Errs map[string]error
+	Errs []ModuleInitErrorDetails
+	msg  string
 }
 
 func (e *ModuleInitError) Error() string {
-	return fmt.Sprintf("\n%d initialization errors", len(e.Errs))
+	if len(e.Errs) == 1 {
+		return fmt.Sprintf("%s - %d problem", e.msg, len(e.Errs))
+	}
+
+	return fmt.Sprintf("%s - %d problems", e.msg, len(e.Errs))
+}
+
+type ModuleInitErrorDetails struct {
+	Dev string
+	Cmd string
+	Mod dut.Module
+	Err error
 }
 
 // Init runs the Init function of all modules for all commands of the provided
@@ -22,7 +34,8 @@ func (e *ModuleInitError) Error() string {
 // the an ModuleInitErr is returned that holds all errors reported by the modules.
 func Init(devices dut.Devlist) error {
 	var ierr = &ModuleInitError{
-		Errs: map[string]error{},
+		Errs: make([]ModuleInitErrorDetails, 0),
+		msg:  "module initialization failed",
 	}
 
 	for devname, device := range devices {
@@ -30,8 +43,12 @@ func Init(devices dut.Devlist) error {
 			for _, module := range cmd.Modules {
 				err := module.Init()
 				if err != nil {
-					m := fmt.Sprintf("dev: %s, cmd: %s, mod: %s", devname, cmdname, module.Config.Name)
-					ierr.Errs[m] = err
+					ierr.Errs = append(ierr.Errs, ModuleInitErrorDetails{
+						Dev: devname,
+						Cmd: cmdname,
+						Mod: module,
+						Err: err,
+					})
 				}
 			}
 		}
@@ -42,6 +59,42 @@ func Init(devices dut.Devlist) error {
 	}
 
 	log.Print("Module Initialization OK")
+
+	return nil
+}
+
+// Denit runs the Deinit function of all modules for all commands of the provided
+// devices. All Deinit functions are called, even if an error occurs. In this case
+// the an ModuleInitErr is returned that holds all errors reported by the modules.
+func Deinit(devices dut.Devlist) error {
+	var derr = &ModuleInitError{
+		Errs: make([]ModuleInitErrorDetails, 0),
+		msg:  "bad clean-up",
+	}
+
+	log.Printf("GRACEFUL SHUTDOWN: De-init modules")
+
+	for devname, device := range devices {
+		for cmdname, cmd := range device.Cmds {
+			for _, module := range cmd.Modules {
+				err := module.Deinit()
+				if err != nil {
+					derr.Errs = append(derr.Errs, ModuleInitErrorDetails{
+						Dev: devname,
+						Cmd: cmdname,
+						Mod: module,
+						Err: err,
+					})
+				}
+			}
+		}
+	}
+
+	if len(derr.Errs) != 0 {
+		return derr
+	}
+
+	log.Print("All modules de-initialized")
 
 	return nil
 }
