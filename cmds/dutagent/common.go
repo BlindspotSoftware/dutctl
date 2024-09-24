18644ba56cb9676a05cdb3beba638628f52f064b
Jens Drenhaus <jens.drenhaus@blindspot.software>
2024-09-24 17:06:41 +0200

    feat: cli for dutctl
    Signed-off-by: Jens Drenhaus <jens.drenhaus@blindspot.software>

---
+++ cmds/dutagent/common.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+
+	"connectrpc.com/connect"
+	"github.com/BlindspotSoftware/dutctl/pkg/dut"
+)
+
+func findCmd(list dut.Devlist, wantDev, wantCmd string) (dut.Device, dut.Command, error) {
+	var (
+		dev dut.Device
+		cmd dut.Command
+	)
+
+	dev, ok := list[wantDev]
+	if !ok {
+		e := connect.NewError(connect.CodeInvalidArgument, fmt.Errorf("device %q does not exist", wantDev))
+
+		return dev, cmd, e
+	}
+
+	cmd, ok = dev.Cmds[wantCmd]
+	if !ok {
+		e := connect.NewError(connect.CodeInvalidArgument, fmt.Errorf("device %q does not have command %q", wantDev, wantCmd))
+
+		return dev, cmd, e
+	}
+
+	if len(cmd.Modules) == 0 {
+		e := connect.NewError(connect.CodeInternal, fmt.Errorf("no modules set for command %q at device %q", wantCmd, wantDev))
+
+		return dev, cmd, e
+	}
+
+	return dev, cmd, nil
+}
