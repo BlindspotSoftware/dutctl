d684bf0be40ecbc9ac5d36abdc4570fde45225aa
Jens Drenhaus <jens.drenhaus@blindspot.software>
2024-10-13 20:40:31 +0200

    refactor: improve queries to dut.Devlist
    Signed-off-by: Jens Drenhaus <jens.drenhaus@blindspot.software>

--- pkg/dut/dut.go
+++ pkg/dut/dut.go
@@ -8,6 +8,7 @@ package dut
 import (
 	"errors"
 	"fmt"
+	"slices"
 	"strings"
 
 	"github.com/BlindspotSoftware/dutctl/pkg/module"
@@ -15,22 +16,35 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
+var (
+	ErrDeviceNotFound  = errors.New("no such device")
+	ErrCommandNotFound = errors.New("no such command")
+	ErrInvalidCommand  = errors.New("command not implemented - no modules or no main module set")
+)
+
 // Devlist is a list of devices-under-test.
 type Devlist map[string]Device
 
+// Names returns the names of all devices in the list.
+// The names are sorted alphabetically.
 func (devs Devlist) Names() []string {
 	names := make([]string, 0, len(devs))
 	for d := range devs {
 		names = append(names, d)
 	}
 
+	slices.Sort(names)
+
 	return names
 }
 
-func (devs Devlist) Cmds(device string) []string {
+// CmdNames returns the names of all commands available for a device or
+// ErrDeviceNotFound if the device is not found. The names are sorted alphabetically.
+// A device with no commands will not report an error but return an empty slice.
+func (devs Devlist) CmdNames(device string) ([]string, error) {
 	dev, ok := devs[device]
 	if !ok {
-		return []string{}
+		return []string{}, ErrDeviceNotFound
 	}
 
 	cmds := make([]string, 0, len(dev.Cmds))
@@ -38,7 +52,31 @@ func (devs Devlist) Cmds(device string) []string {
 		cmds = append(cmds, c)
 	}
 
-	return cmds
+	slices.Sort(cmds)
+
+	return cmds, nil
+}
+
+// FindCmd returns the device and command for a given device and command name.
+// If the device is not found, it returns ErrDeviceNotFound, if the command is not found,
+// it returns ErrCommandNotFound. If the requested command has no modules, or no main module,
+// it returns ErrInvalidCommand.
+func (devs Devlist) FindCmd(device, command string) (Device, Command, error) {
+	dev, ok := devs[device]
+	if !ok {
+		return Device{}, Command{}, ErrDeviceNotFound
+	}
+
+	cmd, ok := dev.Cmds[command]
+	if !ok {
+		return dev, Command{}, ErrCommandNotFound
+	}
+
+	if len(cmd.Modules) == 0 || cmd.countMain() != 1 {
+		return dev, cmd, ErrInvalidCommand
+	}
+
+	return dev, cmd, nil
 }
 
 // Device is the representation of a device-under-test (DUT).
