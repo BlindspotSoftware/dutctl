27a93ab27a631e5d52191fede8cdccc93aff3783
Jens Drenhaus <jens.drenhaus@blindspot.software>
2025-07-25 14:46:04 +0200

    chore: update license to 2025
    Signed-off-by: Jens Drenhaus <jens.drenhaus@blindspot.software>

--- pkg/module/dummy/dummy_file_transfer.go
+++ pkg/module/dummy/dummy_file_transfer.go
@@ -1,4 +1,4 @@
-// Copyright 2024 Blindspot Software
+// Copyright 2025 Blindspot Software
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 package dummy
